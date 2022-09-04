a138b91d92c09cff458260d8ac8857505fa77ce8
aissatou7 <yayefallsaliou@gmail.com>
2022-09-04 16:10:00 +0000

    reproduction site jumia v2

--- tp3.js
+++ tp3.js
@@ -18,13 +18,26 @@ function animation1() {
     
 // }, timeout);
 /**************** silder main *************/
+
+// precedent.addEventListener("click",()=>{
+// mySlide.scrollBy({top:0, left:-150, behavior:"smooth"});
+// });
+// pres();
+// function pres(){
+//   const precedent = document.getElementsByClassName("prev");
+// const next = document.getElementsByClassName('next');
+// var mySlide = document.getElementsByClassName("div_slider");
+//   mySlide.scrollBy({top:0, left:-150, behavior:"smooth"});
+// }
 animation2();
 
 function animation2() {
   var x = document.getElementsByClassName("slider");
+  // const x = document.querySelectorAll('.slider');
   var i;
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
+    console.log(x[0])
   }
   for (i = 0; i < 8; i++) {
     x[i].style.display = "block";
@@ -32,7 +45,8 @@ function animation2() {
    
 }
 function pres() {
-  var x = document.getElementsByClassName("slider");
+  var x = document.getElementsByClassName("div_slider&");
+  // x.style.overflow-x= scroll;
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
     if(i>=3) x[i].style.display = "block";
@@ -40,3 +54,30 @@ function pres() {
 }
 
 }
+
+/**************** silder responsive *************/
+// const sizeR= document.querySelector(html);
+// if (sizeR==280){
+//   animationR();
+// }
+
+// function animationR() {
+//   const x = document.querySelectorAll('.slider');
+//   var i;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   for (i = 0; i < 4; i++) {
+//     x[i].style.display = "block";
+//   }
+   
+// }
+// function presR() {
+//   var x = document.getElementsByClassName("slider");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//     if(i>=3) x[i].style.display = "block";
+ 
+// }
+
+// }
