2836b051c1aca83c6fd6478e9d500b45ac5814b6
aissatou7 <yayefallsaliou@gmail.com>
2022-09-05 13:52:25 +0000

    reproduction site jumia v2

--- tp3.js
+++ tp3.js
@@ -18,43 +18,61 @@ function animation1() {
     
 // }, timeout);
 /**************** silder main *************/
+// animation2();
 
-// precedent.addEventListener("click",()=>{
-// mySlide.scrollBy({top:0, left:-150, behavior:"smooth"});
-// });
-// pres();
-// function pres(){
-//   const precedent = document.getElementsByClassName("prev");
-// const next = document.getElementsByClassName('next');
-// var mySlide = document.getElementsByClassName("div_slider");
-//   mySlide.scrollBy({top:0, left:-150, behavior:"smooth"});
+// function animation2() {
+//   var x = document.getElementsByClassName("slider");
+  // const x = document.querySelectorAll('.slider');
+//   var i;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//     console.log(x[0])
+//   }
+//   for (i = 0; i < 8; i++) {
+//     x[i].style.display = "block";
+//   }
+   
+// }
+// function pres() {
+//   var x = document.getElementsByClassName("div_slider&");
+//   // x.style.overflow-x= scroll;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//     if(i>=3) x[i].style.display = "block";
+ 
+// }
 // }
 animation2();
 
 function animation2() {
   var x = document.getElementsByClassName("slider");
-  // const x = document.querySelectorAll('.slider');
   var i;
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-    console.log(x[0])
   }
   for (i = 0; i < 8; i++) {
     x[i].style.display = "block";
   }
-   
+
 }
 function pres() {
-  var x = document.getElementsByClassName("div_slider&");
-  // x.style.overflow-x= scroll;
+  var x = document.getElementsByClassName("slider");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
     if(i>=3) x[i].style.display = "block";
- 
-}
 
 }
 
+}
+// Bouton Menu responsive
+const boutonMenu = document.querySelector('.boutonM')
+const menu = document.querySelector('.div_matériels')
+
+boutonMenu.addEventListener('click', ()=>
+{
+    menu.style.display = 'block'
+}
+)
 /**************** silder responsive *************/
 // const sizeR= document.querySelector(html);
 // if (sizeR==280){
